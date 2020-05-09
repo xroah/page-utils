df7e2fe9c3be235adcd469558b5ac3fcd840ef21
xroah <396069583@qq.com>
2020-05-09 13:18:01 +0800

    revise popup page

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,25 +1,27 @@
 import { toDataURL } from "qrcode";
+import { getById } from "./utils";
 import "../styles/popup.scss";
 
-const img = document.getElementById("qrCode") as HTMLImageElement;
-const btn = document.getElementById("showPageQRCode") as HTMLButtonElement;
-const clearBtn = document.getElementById("clear") as HTMLButtonElement;
-const currentEl = document.getElementById("onlyCurrent") as HTMLInputElement;
-const historyEl = document.getElementById("history") as HTMLInputElement;
-const cacheEl = document.getElementById("cache") as HTMLInputElement;
-const cookieEl = document.getElementById("cookie") as HTMLInputElement;
-const passwordEl = document.getElementById("password") as HTMLInputElement;
-const downloadEl = document.getElementById("download") as HTMLInputElement;
-const timeRangeEl = document.getElementById("timeRange") as HTMLSelectElement;
+const qrCodeBtn = getById("showPageQRCode") as HTMLButtonElement;
+const clearBtn = getById("clear") as HTMLButtonElement;
+const currentEl = getById("onlyCurrent") as HTMLInputElement;
+const historyEl = getById("history") as HTMLInputElement;
+const cacheEl = getById("cache") as HTMLInputElement;
+const cookieEl = getById("cookie") as HTMLInputElement;
+const passwordEl = getById("password") as HTMLInputElement;
+const downloadEl = getById("download") as HTMLInputElement;
+const timeRangeEl = getById("timeRange") as HTMLSelectElement;
 const OPTIONS_KEY = "clearOptions";
 
-chrome.storage.local.get("options", (opts: any = {}) => {
-    const { normal = {} } = opts.options || {};
+function switchCheckbox() {
+    const onlyCurrent = currentEl.checked;
 
-    if (!!normal.disabled) {
-        return document.body.innerHTML = '<div style="color: #666;">功能未开启</div>'
-    }
+    downloadEl.disabled = onlyCurrent;
+    historyEl.disabled = onlyCurrent;
+    passwordEl.disabled = onlyCurrent;
+}
 
+function initClearOptions() {
     //read saved options
     chrome.storage.local.get(OPTIONS_KEY, (opts: any = {}) => {
         opts = opts[OPTIONS_KEY] || {};
@@ -37,113 +39,129 @@ chrome.storage.local.get("options", (opts: any = {}) => {
 
         switchCheckbox();
     });
+}
 
-    btn.addEventListener("click", function () {
-        const imgWrapper = img.parentNode as HTMLElement;
+function toggleQRCode() {
+    const img = getById("qrCode") as HTMLImageElement;
+    const imgWrapper = img.parentNode as HTMLElement;
 
-        imgWrapper.classList.toggle("d-none");
+    imgWrapper.classList.toggle("d-none");
 
-        if (imgWrapper.classList.contains("d-none")) {
-            this.innerHTML = "显示页面二维码";
-        } else {
-            this.innerHTML = "隐藏页面二维码";
-        }
+    if (imgWrapper.classList.contains("d-none")) {
+        this.innerHTML = "显示页面二维码";
+    } else {
+        this.innerHTML = "隐藏页面二维码";
+    }
 
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, (tabs: Array<chrome.tabs.Tab>) => {
-            toDataURL(tabs[0].url, {
-                width: 160,
-                margin: 0
-            }, (error: Error, url: string) => {
-                if (error) console.error(error);
-                img.src = url;
-            });
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, (tabs: Array<chrome.tabs.Tab>) => {
+        toDataURL(tabs[0].url, {
+            width: 160,
+            margin: 0
+        }, (error: Error, url: string) => {
+            if (error) console.error(error);
+            img.src = url;
         });
     });
+}
 
-    function switchCheckbox() {
+function saveOptions() {
+    chrome.storage.local.set({
+        [OPTIONS_KEY]: {
+            timeRange: timeRangeEl.value,
+            current: currentEl.checked,
+            history: historyEl.checked,
+            cache: cacheEl.checked,
+            cookie: cookieEl.checked,
+            password: passwordEl.checked,
+            download: passwordEl.checked
+        }
+    });
+}
+
+function clear() {
+    const history = historyEl.checked;
+    const cache = cacheEl.checked;
+    const cookie = cookieEl.checked;
+    const password = passwordEl.checked;
+    const download = downloadEl.checked;
+
+    if (
+        !history &&
+        !cache &&
+        !cookie &&
+        !password &&
+        !download
+    ) return;
+
+    chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    }, tabs => {
+        const tab = tabs[0];
+        const url = tab.url;
+        const timeMap = new Map();
         const onlyCurrent = currentEl.checked;
+        const others = cookie && !onlyCurrent;
+        const mask = getById("cleaning");
+        const stat = mask.firstElementChild;
 
-        downloadEl.disabled = onlyCurrent;
-        historyEl.disabled = onlyCurrent;
-        passwordEl.disabled = onlyCurrent;
+        timeMap.set("h", 60 * 60 * 1000);
+        timeMap.set("d", 24 * 60 * 60 * 1000);
+        timeMap.set("w", 7 * 24 * 60 * 60 * 1000);
+        timeMap.set("m", 4 * 7 * 24 * 60 * 60 * 1000);
+        timeMap.set("a", 0);
+        saveOptions();
+
+        if (!/^https?/.test(url) && onlyCurrent) return;
+
+        const a = document.createElement("a");
+        a.href = url;
+        clearBtn.disabled = true;
+
+        stat.classList.remove("complete");
+
+        mask.style.display = "flex";
+        
+        chrome.browsingData.remove(
+            {
+                since: timeMap.get(timeRangeEl.value) || 0,
+                origins: onlyCurrent ? [a.origin] : undefined //since chrome 74
+            } as any,
+            {
+                cache,
+                cookies: cookie,
+                downloads: download && !onlyCurrent,
+                formData: others,
+                indexedDB: others,
+                localStorage: others,
+                passwords: password && !onlyCurrent,
+                history: history && !onlyCurrent
+            },
+            () => {
+                stat.classList.add("complete");
+                setTimeout(() => mask.style.display = "none", 1500);
+                clearBtn.disabled = false;
+            });
+    });
+}
+
+chrome.storage.local.get("options", (opts: any = {}) => {
+    const { normal = {} } = opts.options || {};
+
+    if (!!normal.disabled) {
+        return document.body.innerHTML = '<div style="color: #666;">功能未开启</div>'
     }
 
-    function saveOptions() {
-        chrome.storage.local.set({
-            [OPTIONS_KEY]: {
-                current: currentEl.checked,
-                history: historyEl.checked,
-                cache: cacheEl.checked,
-                cookie: cookieEl.checked,
-                password: passwordEl.checked,
-                download: passwordEl.checked
-            }
-        });
-    }
+    initClearOptions();
 
+    qrCodeBtn.addEventListener("click", toggleQRCode);
     currentEl.addEventListener("change", switchCheckbox);
-
-    clearBtn.addEventListener("click", () => {
-        const history = historyEl.checked;
-        const cache = cacheEl.checked;
-        const cookie = cookieEl.checked;
-        const password = passwordEl.checked;
-        const download = downloadEl.checked;
-
-        if (
-            !history &&
-            !cache &&
-            !cookie &&
-            !password &&
-            !download
-        ) return;
-
-        chrome.tabs.query({
-            currentWindow: true,
-            active: true
-        }, tabs => {
-            const tab = tabs[0];
-            const url = tab.url;
-            const timeMap = new Map();
-            const onlyCurrent = currentEl.checked;
-            const others = cookie && !onlyCurrent;
-
-            timeMap.set("h", 60 * 60 * 1000);
-            timeMap.set("d", 24 * 60 * 60 * 1000);
-            timeMap.set("w", 7 * 24 * 60 * 60 * 1000);
-            timeMap.set("m", 4 * 7 * 24 * 60 * 60 * 1000);
-            timeMap.set("a", 0);
-            saveOptions();
-
-            if (!/^https?/.test(url) && onlyCurrent) return;
-
-            const a = document.createElement("a");
-            a.href = url;
-            clearBtn.innerHTML = "正在清理...";
-            clearBtn.disabled = true;
-
-            chrome.browsingData.remove(
-                {
-                    since: timeMap.get(timeRangeEl.value) || 0,
-                    origins: onlyCurrent ? [a.origin] : undefined
-                } as any,
-                {
-                    cache,
-                    cookies: cookie,
-                    downloads: download && !onlyCurrent,
-                    formData: others,
-                    indexedDB: others,
-                    localStorage: others,
-                    passwords: password && !onlyCurrent,
-                    history: history && !onlyCurrent
-                },
-                () => {
-                    clearBtn.innerHTML = "清理";
-                    clearBtn.disabled = false;
-                });
-        });
+    clearBtn.addEventListener("click", clear);
+    getById("settings").addEventListener("click", evt => {
+        chrome.runtime.openOptionsPage();
+        evt.preventDefault();
     });
 });
